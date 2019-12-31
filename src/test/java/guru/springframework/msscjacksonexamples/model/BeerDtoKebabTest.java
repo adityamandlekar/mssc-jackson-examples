a64639b4a6f95c9496c7c15a7bf17a2e4a3a9a64
Aditya Mandlekar <aditya.mandlekar@thomsonreuters.com>
2019-12-31 05:39:22 -0500

    Assignment Jackson Property Naming Strategies- Kebab & Snake

--- src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoKebabTest.java
+++ src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoKebabTest.java
@@ -7,19 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.test.context.ActiveProfiles;
 
-/**
- * Created by jt on 2019-06-02.
- */
-@ActiveProfiles("snake")
+@ActiveProfiles("kebab")
 @JsonTest
-public class BeerDtoSnakeTest extends BaseTest {
+public class BeerDtoKebabTest extends BaseTest {
 
     @Test
-    void testSnake() throws JsonProcessingException {
+    void testkebab() throws JsonProcessingException {
         BeerDto dto = getDto();
-
         String json = objectMapper.writeValueAsString(dto);
-
         System.out.println(json);
     }
+
+
 }
